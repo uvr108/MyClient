cb717337f5020ac25487a57f59a4c81f4f82ec6d
Ulises Vergara <uvergara@csn.uchile.cl>
2019-12-28 16:58:11 -0300

    Se eliminaron comentarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddIssueComponent } from './components/add-issue/add-issue.component';
+import { EditIssueComponent } from './components/edit-issue/edit-issue.component';
+import { IssueListComponent } from './components/issue-list/issue-list.component';
+
+import { CrudService } from './shared/crud.service';
+
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddIssueComponent,
+    EditIssueComponent,
+    IssueListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [CrudService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
