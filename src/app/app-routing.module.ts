456920e3ec44ebe64ab47fb84d6b3226617780b7
Ulises Vergara <uvergara@csn.uchile.cl>
2019-12-28 16:57:30 -0300

    Se eliminaron comentarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IssueListComponent } from './components/issue-list/issue-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'presupuestos', component: IssueListComponent, data: {table: 'presupuestos'}},
+  {path : 'items', component: IssueListComponent, data: {table: 'items'}}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
