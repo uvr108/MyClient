ffd5fda0f98b3acfc983e81983706dfd36dba79e
uvergara <uvergara@csn.uchile.cl>
2020-01-16 18:02:17 -0300

    Se mejora recursividad

--- src/app/tabla.ts
+++ src/app/tabla.ts
@@ -1,3 +1,5 @@
 export const Presupuesto: Array<string> = ['name','monto'];
-export const Item: Array<string> = ['name','monto'];
-export const Subitem: Array<string> = ['name','monto'];
+export const Item: Array<string> = ['name','presupuestoId','monto'];
+export const Subitem: Array<string> = ['name','itemId','monto'];
+
+export const NAVEGA: Array<Array<string>> = [Presupuesto, Item, Subitem];
