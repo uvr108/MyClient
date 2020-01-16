ffb23f9dcd4d996fbdc110d2293a87286dd96abc
uvergara <uvergara@csn.uchile.cl>
2020-01-16 18:01:55 -0300

    Se mejora recursividad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { FormBuilder } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddIssueComponent } from './components/add-issue/add-issue.component';
-import { EditIssueComponent } from './components/edit-issue/edit-issue.component';
-import { IssueListComponent } from './components/issue-list/issue-list.component';
-
+import { ListComponent } from './components/list/list.component';
 import { CrudService } from './shared/crud.service';
 import { HttpClientModule } from '@angular/common/http';
 import { TreeComponent } from './components/tree/tree.component';
@@ -16,9 +12,7 @@ import { TopBarComponent } from './components/top-bar/top-bar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AddIssueComponent,
-    EditIssueComponent,
-    IssueListComponent,
+    ListComponent,
     TreeComponent,
     TopBarComponent],
   imports: [
