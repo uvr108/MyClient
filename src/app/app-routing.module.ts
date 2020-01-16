0cdf2af98a411fd26b3fe56ec8fafe15180644e1
uvergara <uvergara@csn.uchile.cl>
2020-01-16 18:01:41 -0300

    Se mejora recursividad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { IssueListComponent } from './components/issue-list/issue-list.component';
+import { ListComponent } from './components/list/list.component';
 import { TreeComponent } from './components/tree/tree.component';
 
 
 const routes: Routes = [
-  {path : 'presupuestos', component: IssueListComponent, data: {table: 'presupuestos'}},
-  {path : 'items', component: IssueListComponent, data: {table: 'items'}},
-  {path : 'item/:id', component: TreeComponent},
-  {path : 'subitems', component: IssueListComponent, data: {table: 'subitems'}},
-  {path : 'subitem/:id', component: IssueListComponent, data: {table: 'subitems'}},
+  {path : 'presupuestos', component: ListComponent, data: {table: 'presupuestos'}},
+  {path : 'items', component: ListComponent, data: {table: 'items'}},
+  // {path : 'item/:id', component: TreeComponent},
+  {path : 'subitems', component: ListComponent, data: {table: 'subitems'}},
+  // {path : 'subitem/:id', component: IssueListComponent, data: {table: 'subitems'}},
   {path : 'tree', component: TreeComponent, data: {}}
 ];
 
