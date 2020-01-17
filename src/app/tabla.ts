b9f3acd3085eb53fdd0cf34e6334e481211ac1e2
uvergara <uvergara@csn.uchile.cl>
2020-01-17 16:22:33 -0300

    Se mejoro parametrizacionb

--- src/app/tabla.ts
+++ src/app/tabla.ts
@@ -1,5 +1,6 @@
-export const Presupuesto: Array<string> = ['name','monto'];
+export const Presupuesto: Array<string> = ['id','name','monto'];
 export const Item: Array<string> = ['name','presupuestoId','monto'];
 export const Subitem: Array<string> = ['name','itemId','monto'];
 
 export const NAVEGA: Array<Array<string>> = [Presupuesto, Item, Subitem];
+export const TABLAS: Array<string> = ['presupuestos','items','subitems'];
