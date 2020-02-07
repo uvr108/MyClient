b4283862117d0ea6d0db0036ade523918d56a1a2
uvergara <uvergara@csn.uchile.cl>
2020-02-07 17:40:27 -0300

    arreglos

--- src/app/tabla.ts
+++ src/app/tabla.ts
@@ -1,6 +1,6 @@
-export const Presupuesto: Array<string> = ['id','name','monto'];
-export const Item: Array<string> = ['name','presupuestoId','monto'];
-export const Subitem: Array<string> = ['name','itemId','monto'];
+export const presupuestos: Array<string> = ['id','name','monto'];
+export const items: Array<string> = ['name','presupuestoId','monto'];
+export const subitems: Array<string> = ['name','itemId','monto'];
 
-export const NAVEGA: Array<Array<string>> = [Presupuesto, Item, Subitem];
+export const NAVEGA: Array<Array<string>> = [presupuestos, items, subitems];
 export const TABLAS: Array<string> = ['presupuestos','items','subitems'];
